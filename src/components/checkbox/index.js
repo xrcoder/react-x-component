66858138343bb52796877e1d85b13cf9b0fa468b
洪锋 <hongfeng@megvii.com>
2019-05-29 16:41:48 +0800

    checkbox modify

--- src/components/checkbox/index.js
+++ src/components/checkbox/index.js
@@ -7,91 +7,93 @@ class XCheckbox extends Component {
 
     static propTypes = {
         selectedValueList: PropTypes.array,
-        checkboxList:PropTypes.array
+        checkboxList: PropTypes.array
     };
-    
-    constructor(props){
+
+    constructor(props) {
         super(props)
-        this.state={
-            selectedValueList:props.selectedValueList||[],
-            selectedList:[],
-            checkboxList:props.checkboxList
+        this.state = {
+            selectedValueList: props.selectedValueList || [],
+            selectedList: [],
+            checkboxList: props.checkboxList
         }
     }
 
-    componentDidMount(){
-        let arr= [];
-        this.state.checkboxList.map(item=>{
-            this.state.selectedValueList.map(item_=>{
-                if(item_&&item.value==item_){
-                    item.selected=true;
+    componentDidMount() {
+        let arr = [];
+        let checkboxList = JSON.parse(JSON.stringify(this.state.selectedList))
+        let selectedValueList = JSON.parse(JSON.stringify(this.state.selectedValueList))
+        checkboxList.map(item => {
+            selectedValueList.map(item_ => {
+                if (item_ && item.value == item_) {
+                    item.selected = true;
                     arr.push(item);
-                }else {
+                } else {
                     item.selected = false;
                 }
             })
         })
-        console.log(this.state,999)
+        console.log(this.state, 999)
         this.setState({
-            selectedList:arr
+            selectedList: arr
         })
-        
+
     }
 
-    componentWillUnmount(){
+    componentWillUnmount() {
         this.setState({
-            selectedValueList:[],
-            selectedList:[],
-            checkboxList:this.props.checkboxList
+            selectedValueList: [],
+            selectedList: [],
+            checkboxList: this.props.checkboxList
         })
     }
 
-    render(){
+    render() {
         return (
             <div className="x-checkbox">
                 {
-                    this.state.checkboxList.map((item,index)=>{
+                    this.state.checkboxList.map((item, index) => {
                         return (
-                            <div key={item.label+index} className={`x-checkbox-li ${item.selected?'active-checkbox':null} ${item.disable==true?'checkbox-disable':null}`} onClick={()=>{
+                            <div key={item.label + index} className={`x-checkbox-li ${item.selected ? 'active-checkbox' : null} ${item.disable == true ? 'checkbox-disable' : null}`} onClick={() => {
 
-                                if(item.disable){
+                                if (item.disable) {
                                     return
                                 }
-                                
+
                                 let arr = JSON.parse(JSON.stringify(this.state.selectedList))
                                 let selected = false;
                                 let selected_index;
-                                arr.length&&arr.map((item_,index)=>{
-                                    if(item_.value==item.value&&item_.label==item.label){
+                                arr.length && arr.map((item_, index) => {
+                                    if (item_.value == item.value && item_.label == item.label) {
                                         selected = true;
                                         selected_index = index
                                     }
                                 })
 
                                 // let arr = JSON.parse(JSON.stringify(this.state.selectedList))
-                                if(selected){
+                                if (selected) {
                                     item.selected = false;
-                                    arr.splice(selected_index,1)
+                                    arr.splice(selected_index, 1)
                                     this.setState({
-                                        selectedList:arr
+                                        selectedList: arr
                                     })
-                                }else{
+                                } else {
                                     item.selected = true;
                                     arr.push(item);
                                     this.setState({
                                         selectedList: arr
                                     })
                                 }
-                               
+
                                 this.props.onChange(arr)
                             }}>
-                            {
-                                item.selected==true?
-                                <XIcon type='checkbox-selected'></XIcon> : <XIcon type='checkbox'></XIcon>
-                            }
-                            {item.label}
-                            
-                            </div>    
+                                {
+                                    item.selected == true ?
+                                        <XIcon type='checkbox-selected'></XIcon> : <XIcon type='checkbox'></XIcon>
+                                }
+                                {item.label}
+
+                            </div>
                         )
                     })
                 }
