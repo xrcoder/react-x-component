efd854d242557c5e11647cbd6412b4e96300710a
lilei <289670502@qq.com>
2019-07-15 21:29:38 +0800

    'opt'

--- example/src/pages/app/input/form.js
+++ example/src/pages/app/input/form.js
@@ -14,41 +14,79 @@ export default class extends React.Component {
 
     render() {
         return (
-            <Box>
-                <Box.Header>表单</Box.Header>
-                <Box.Body>
-                    <Row>
-                        <Col md={12}>
-                            <Form.Group>
-                                <Form.Name>邮件</Form.Name>
-                                <Input/>
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Name>账号</Form.Name>
-                                <Input/>
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Name>密码</Form.Name>
-                                <Input.Clear type={'password'} onChange={(res) => {
-                                    console.log(res);
-                                }} onEnter={(res) => {
-                                    console.log(res)
-                                }}/>
-                            </Form.Group>
-                            <Form.Group>
-                                <Checkbox.Item
-                                    label={'记住密码'}
-                                    value={this.state.isMark}
-                                    onChange={(e, res) => {
-                                        this.setState({
-                                            isMark: res
-                                        })
+            <>
+                <Box>
+                    <Box.Header>垂直表单</Box.Header>
+                    <Box.Body>
+                        <Row>
+                            <Col md={12}>
+                                <Form.Group>
+                                    <Form.Name>邮件</Form.Name>
+                                    <Input/>
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Name>账号</Form.Name>
+                                    <Input/>
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Name>密码</Form.Name>
+                                    <Input.Clear type={'password'} onChange={(res) => {
+                                        console.log(res);
+                                    }} onEnter={(res) => {
+                                        console.log(res)
                                     }}/>
-                            </Form.Group>
-                        </Col>
-                    </Row>
-                </Box.Body>
-            </Box>
+                                </Form.Group>
+                                <Form.Group>
+                                    <Checkbox.Item
+                                        label={'记住密码'}
+                                        value={this.state.isMark}
+                                        onChange={(e, res) => {
+                                            this.setState({
+                                                isMark: res
+                                            })
+                                        }}/>
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                    </Box.Body>
+                </Box>
+                <Box>
+                    <Box.Header>水平表单</Box.Header>
+                    <Box.Body>
+                        <Row>
+                            <Col md={12}>
+                                <Form.Group horizontal>
+                                    <Form.Name>访客姓名</Form.Name>
+                                    <Input/>
+                                </Form.Group>
+                                <Form.Group horizontal>
+                                    <Form.Name>证件号码</Form.Name>
+                                    <Input/>
+                                </Form.Group>
+                                <Form.Group horizontal>
+                                    <Form.Name>身份证号码</Form.Name>
+                                    <Input.Clear type={'password'} onChange={(res) => {
+                                        console.log(res);
+                                    }} onEnter={(res) => {
+                                        console.log(res)
+                                    }}/>
+                                </Form.Group>
+                                <Form.Group horizontal>
+                                    <Form.Name></Form.Name>
+                                    <Checkbox.Item
+                                        label={'记住密码'}
+                                        value={this.state.isMark}
+                                        onChange={(e, res) => {
+                                            this.setState({
+                                                isMark: res
+                                            })
+                                        }}/>
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                    </Box.Body>
+                </Box>
+            </>
         )
     }
 }
