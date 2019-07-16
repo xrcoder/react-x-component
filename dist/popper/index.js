10f773258be644badf18e0500fb2796b289de7ae
lilei <289670502@qq.com>
2019-07-16 14:53:02 +0800

    'opt'

---
+++ dist/popper/index.js
@@ -0,0 +1,49 @@
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(["exports", "react", "rc-tooltip"], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports, require("react"), require("rc-tooltip"));
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports, global.react, global.rcTooltip);
+    global.index = mod.exports;
+  }
+})(this, function (_exports, _react, _rcTooltip) {
+  "use strict";
+
+  var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+  Object.defineProperty(_exports, "__esModule", {
+    value: true
+  });
+  _exports["default"] = void 0;
+  _react = _interopRequireDefault(_react);
+  _rcTooltip = _interopRequireDefault(_rcTooltip);
+
+  function Popper(props) {
+    var children = props.children,
+        placement = props.placement,
+        content = props.content,
+        trigger = props.trigger,
+        overlayClassName = props.overlayClassName,
+        mouseEnterDelay = props.mouseEnterDelay,
+        mouseLeaveDelay = props.mouseLeaveDelay,
+        overlayStyle = props.overlayStyle;
+    return _react["default"].createElement(_rcTooltip["default"], {
+      overlayClassName: overlayClassName,
+      placement: placement,
+      trigger: trigger,
+      prefixCls: 'x-popper',
+      mouseLeaveDelay: mouseLeaveDelay,
+      mouseEnterDelay: mouseEnterDelay,
+      overlayStyle: overlayStyle,
+      overlay: content
+    }, children);
+  }
+
+  var _default = Popper;
+  _exports["default"] = _default;
+  module.exports = exports.default;
+});
