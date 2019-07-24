6851275a367b4c6aff7ab46c4474b6142e5d519e
lilei <289670502@qq.com>
2019-07-24 15:40:00 +0800

    'opt'

--- example/src/pages/app/toast/index.js
+++ example/src/pages/app/toast/index.js
@@ -11,24 +11,26 @@ export default class extends React.Component {
                         <Box>
                             <Box.Header>通知</Box.Header>
                             <Box.Body>
-                                <Button onClick={() => {
-                                    Toast.push("智能盒子创建成功，请前往关联!")
-                                }}>默认类型</Button>
-                                <Button onClick={() => {
-                                    Toast.push("智能盒子创建成功，请前往关联!", {
-                                        type: 'success'
-                                    })
-                                }}>成功消息</Button>
-                                <Button onClick={() => {
-                                    Toast.push("智能盒子创建成功，请前往关联!", {
-                                        type: 'error'
-                                    })
-                                }}>错误提示</Button>
-                                <Button onClick={() => {
-                                    Toast.push("智能盒子创建成功，请前往关联!", {
-                                        type: 'warning'
-                                    })
-                                }}>警告提示</Button>
+                                <Button.Box align={'left'}>
+                                    <Button onClick={() => {
+                                        Toast.push("智能盒子创建成功，请前往关联!")
+                                    }}>默认类型</Button>
+                                    <Button onClick={() => {
+                                        Toast.push("智能盒子创建成功，请前往关联!", {
+                                            type: 'success'
+                                        })
+                                    }}>成功消息</Button>
+                                    <Button onClick={() => {
+                                        Toast.push("智能盒子创建成功，请前往关联!", {
+                                            type: 'error'
+                                        })
+                                    }}>错误提示</Button>
+                                    <Button onClick={() => {
+                                        Toast.push("智能盒子创建成功，请前往关联!", {
+                                            type: 'warning'
+                                        })
+                                    }}>警告提示</Button>
+                                </Button.Box>
                             </Box.Body>
                             <Box.Footer>盒子Footer（尾部）</Box.Footer>
                         </Box>
@@ -37,12 +39,14 @@ export default class extends React.Component {
                         <Box>
                             <Box.Header>消息</Box.Header>
                             <Box.Body>
-                                <Button onClick={() => {
-                                    Toast.notify("智能盒子创建成功，请前往关联!")
-                                }}>默认类型</Button>
-                                <Button onClick={() => {
-                                    Toast.notify("智能盒子创建成功，请前往关联!", 'error')
-                                }}>默认类型</Button>
+                                <Button.Box align={'left'}>
+                                    <Button onClick={() => {
+                                        Toast.notify("智能盒子创建成功，请前往关联!")
+                                    }}>默认类型</Button>
+                                    <Button onClick={() => {
+                                        Toast.notify("智能盒子创建成功，请前往关联!", 'error')
+                                    }}>默认类型</Button>
+                                </Button.Box>
                             </Box.Body>
                             <Box.Footer>盒子Footer（尾部）</Box.Footer>
                         </Box>
