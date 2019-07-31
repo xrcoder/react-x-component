514cf8186f4a6ec07df836a05c7b607e018b0661
lilei <289670502@qq.com>
2019-07-31 17:01:07 +0800

    'opt'

--- dist/remind/index.js
+++ dist/remind/index.js
@@ -1,16 +1,16 @@
 (function (global, factory) {
   if (typeof define === "function" && define.amd) {
-    define(["exports", "react", "./loading"], factory);
+    define(["exports", "react", "./loading", "./avatar"], factory);
   } else if (typeof exports !== "undefined") {
-    factory(exports, require("react"), require("./loading"));
+    factory(exports, require("react"), require("./loading"), require("./avatar"));
   } else {
     var mod = {
       exports: {}
     };
-    factory(mod.exports, global.react, global.loading);
+    factory(mod.exports, global.react, global.loading, global.avatar);
     global.index = mod.exports;
   }
-})(this, function (_exports, _react, _loading) {
+})(this, function (_exports, _react, _loading, _avatar) {
   "use strict";
 
   var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
@@ -21,12 +21,14 @@
   _exports["default"] = void 0;
   _react = _interopRequireDefault(_react);
   _loading = _interopRequireDefault(_loading);
+  _avatar = _interopRequireDefault(_avatar);
 
   function Remind() {
     return null;
   }
 
   Remind.Loading = _loading["default"];
+  Remind.Avatar = _avatar["default"];
   var _default = Remind;
   _exports["default"] = _default;
   module.exports = exports.default;
