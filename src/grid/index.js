37727f18b6694ee019ef5849bab8e07f432cfc2c
lilei <289670502@qq.com>
2019-08-05 21:01:34 +0800

    'opt'

--- src/grid/index.js
+++ src/grid/index.js
@@ -2,24 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-export function Row({children, className, style}) {
-
+export function Row({children, className, space, style}) {
     return (
-        <div className={classnames('x-row', className)} style={style}>
-            {children}
+        <div className={classnames('x-row', className)}
+             style={style}
+             css={{
+                 marginRight: `-${space}px`,
+                 marginLeft: `-${space}px`
+             }}>
+            {
+                React.Children.map(children, (child) => {
+                    return <child.type space={space} {...child.props}/>;
+                })
+            }
         </div>
     );
-
 }
 
-export function Col({children, sm, md, lg, className, style}) {
+export function Col({children, sm, md, lg, space, className, style}) {
 
     let _sm = sm ? `col-sm-${sm}` : '',
         _md = md ? `col-md-${md}` : '',
         _lg = lg ? `col-lg-${lg}` : '';
 
     return (
-        <div className={classnames(_sm, _md, _lg, className)} style={style}>
+        <div className={classnames(_sm, _md, _lg, className)}
+             style={style}
+             css={{paddingLeft: `${space}px`, paddingRight: `${space}px`}}>
             {children}
         </div>
     );
@@ -27,12 +36,14 @@ export function Col({children, sm, md, lg, className, style}) {
 
 Row.propTypes = {
     className: PropTypes.string,
-    style: PropTypes.object
+    style: PropTypes.object,
+    space: PropTypes.number
 };
 
 Row.defaultProps = {
     className: '',
-    style: null
+    style: null,
+    space: 15
 };
 
 Col.propTypes = {
