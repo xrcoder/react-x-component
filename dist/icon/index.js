4b8ddb8fae5657c0f13b7506cb10cde357ac6f59
lilei <289670502@qq.com>
2019-08-09 11:34:13 +0800

    'opt'

--- dist/icon/index.js
+++ dist/icon/index.js
@@ -1,16 +1,16 @@
 (function (global, factory) {
   if (typeof define === "function" && define.amd) {
-    define(["exports", "@emotion/core", "react", "classnames", "./demo"], factory);
+    define(["exports", "@emotion/core", "react", "classnames"], factory);
   } else if (typeof exports !== "undefined") {
-    factory(exports, require("@emotion/core"), require("react"), require("classnames"), require("./demo"));
+    factory(exports, require("@emotion/core"), require("react"), require("classnames"));
   } else {
     var mod = {
       exports: {}
     };
-    factory(mod.exports, global.core, global.react, global.classnames, global.demo);
+    factory(mod.exports, global.core, global.react, global.classnames);
     global.index = mod.exports;
   }
-})(this, function (_exports, _core, _react, _classnames, _demo) {
+})(this, function (_exports, _core, _react, _classnames) {
   "use strict";
 
   var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
