5f817c6c2266de801cf83d915ebcefec5414bdfe
luokaihui <luokaihui@megvii.com>
2019-11-11 10:28:12 +0800

    feat:select api doc

--- example/src/pages/doc/select/api.js
+++ example/src/pages/doc/select/api.js
@@ -3,9 +3,6 @@ export default {
         headConf: ['Name', 'Type', 'Default', 'Description'],
         bodyConf: [
             {
-                Name: 'size', Type: 'string', Default: '', Description: ''
-            },
-            {
                 Name: 'className', Type: 'string', Default: '', Description: 'select自定义样式类名'
             },
             {
@@ -18,31 +15,96 @@ export default {
                 Name: 'value', Type: 'object', Default: 'null', Description: '选中的值'
             },
             {
-                Name: 'placeholder', Type: 'string', Default: '', Description: ''
+                Name: 'placeholder', Type: 'string', Default: '\'\'', Description: '初始化的时候占位文字描述'
             },
             {
-                Name: 'noOptionsMessage', Type: 'function', Default: '', Description: ''
+                Name: 'noOptionsMessage', Type: 'function', Default: '-', Description: '没有任何选项的时候，下拉框显示的文字'
             },
             {
-                Name: 'options', Type: 'array', Default: '', Description: ''
+                Name: 'options', Type: 'array', Default: 'null', Description: '选择框的所有选项列表，格式：[{value: 0, label: \'选项1\'}]'
             },
             {
-                Name: 'menuIsOpen', Type: 'boolean', Default: '', Description: ''
+                Name: 'menuIsOpen', Type: 'boolean', Default: 'false', Description: '下拉框是否常开'
             },
             {
-                Name: 'menuPlacement', Type: 'string', Default: '', Description: ''
+                Name: 'menuPlacement', Type: 'string', Default: 'bottom', Description: '下拉框出现的位置，可取一下值：(auto | bottom | top)'
             },
             {
-                Name: '', Type: '', Default: '', Description: ''
+                Name: 'isRtl', Type: 'boolean', Default: 'false', Description: '文字是否从右到左展示'
+            },
+            {
+                Name: 'onChange', Type: 'function', Default: '-', Description: '选项发生变化后的回调函数，参数是被选择的选项'
             }
         ]
     },
     multipleApi: {
         headConf: ['Name', 'Type', 'Default', 'Description'],
-        bodyConf: []
+        bodyConf: [
+            {
+                Name: 'className', Type: 'string', Default: '', Description: 'select自定义样式类名'
+            },
+            {
+                Name: 'disabled', Type: 'boolean', Default: 'false', Description: '是否禁用，默认false'
+            },
+            {
+                Name: 'value', Type: 'object', Default: 'null', Description: '选中的值'
+            },
+            {
+                Name: 'placeholder', Type: 'string', Default: '\'\'', Description: '初始化的时候占位文字描述'
+            },
+            {
+                Name: 'noOptionsMessage', Type: 'function', Default: '-', Description: '没有任何选项的时候，下拉框显示的文字'
+            },
+            {
+                Name: 'options', Type: 'array', Default: 'null', Description: '选择框的所有选项列表，格式：[{value: 0, label: \'选项1\'}]'
+            },
+            {
+                Name: 'menuIsOpen', Type: 'boolean', Default: 'false', Description: '下拉框是否常开'
+            },
+            {
+                Name: 'menuPlacement', Type: 'string', Default: 'bottom', Description: '下拉框出现的位置，可取一下值：(auto | bottom | top)'
+            },
+            {
+                Name: 'isRtl', Type: 'boolean', Default: 'false', Description: '文字是否从右到左展示'
+            },
+            {
+                Name: 'onChange', Type: 'function', Default: '-', Description: '选项发生变化后的回调函数，参数是被选择的选项'
+            }
+        ]
     },
     searchApi: {
         headConf: ['Name', 'Type', 'Default', 'Description'],
-        bodyConf: []
+        bodyConf: [
+            {
+                Name: 'className', Type: 'string', Default: '', Description: 'select自定义样式类名'
+            },
+            {
+                Name: 'disabled', Type: 'boolean', Default: 'false', Description: '是否禁用，默认false'
+            },
+            {
+                Name: 'value', Type: 'object', Default: 'null', Description: '选中的值'
+            },
+            {
+                Name: 'placeholder', Type: 'string', Default: '\'\'', Description: '初始化的时候占位文字描述'
+            },
+            {
+                Name: 'noOptionsMessage', Type: 'function', Default: '-', Description: '没有任何选项的时候，下拉框显示的文字'
+            },
+            {
+                Name: 'options', Type: 'array', Default: 'null', Description: '选择框的所有选项列表，格式：[{value: 0, label: \'选项1\'}]'
+            },
+            {
+                Name: 'menuIsOpen', Type: 'boolean', Default: 'false', Description: '下拉框是否常开'
+            },
+            {
+                Name: 'menuPlacement', Type: 'string', Default: 'bottom', Description: '下拉框出现的位置，可取一下值：(auto | bottom | top)'
+            },
+            {
+                Name: 'isRtl', Type: 'boolean', Default: 'false', Description: '文字是否从右到左展示'
+            },
+            {
+                Name: 'onChange', Type: 'function', Default: '-', Description: '选项发生变化后的回调函数，参数是被选择的选项'
+            }
+        ]
     }
 }
