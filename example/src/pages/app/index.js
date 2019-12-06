3ba98dd3d3b6a2cfac2eeaedcbd0c2844ce2c5e6
luokaihui <luokaihui@megvii.com>
2019-12-06 14:04:41 +0800

    feat:add homepage

--- example/src/pages/app/index.js
+++ example/src/pages/app/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import {LayoutB} from 'react-x-component';
-import {Route, Switch} from 'react-router-dom';
+import { LayoutB } from 'react-x-component';
+import { Route, Switch } from 'react-router-dom';
 import Router from './router';
 import AppHeader from './common/header';
 import AppSider from './common/sider';
 
-const {Sider, Container, Header, Content, Footer} = LayoutB;
+const { Sider, Container, Header, Content, Footer } = LayoutB;
 
 function Main() {
     return (
         <Container>
             <Sider className="app-sider">
-                <AppSider prefix="app/component"/>
+                <AppSider prefix="app/component" />
             </Sider>
             <Content className="app-content">
-                <Router/>
+                <Router />
             </Content>
         </Container>
     )
@@ -27,15 +27,16 @@ export default class extends React.Component {
     }
 
     render() {
-        const {match} = this.props;
+        const { match } = this.props;
         return (
             <LayoutB>
                 <Header className="app-header">
-                    <AppHeader/>
+                    <AppHeader />
                 </Header>
                 <Switch>
-                    <Route path={`${match.url}/component`} component={Main}/>
-                    <Route path={`${match.url}/doc`} component={require('../doc')}/>
+                    <Route path={`${match.url}/component`} component={Main} />
+                    <Route path={`${match.url}/doc`} component={require('../doc')} />
+                    <Route path={`${match.url}/home`} component={require('../home')} />
                 </Switch>
             </LayoutB>
         )
--- example/src/pages/home/index.js
+++ example/src/pages/home/index.js
@@ -1,12 +1,20 @@
-import React, {Fragment} from 'react';
-import {withRouter, Link} from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { withRouter, Link } from 'react-router-dom';
+import { Box } from 'react-x-component';
 
 @withRouter
 export default class extends React.Component {
 
     render() {
         return (
-            <div>首页 <Link to="/app/doc">返回</Link></div>
+            <div className="x-component-homepage">
+                <Box>
+                    <Box.Header></Box.Header>
+                    <Box.Body>
+                        <div>首页 <Link to="/app/doc">返回</Link></div>
+                    </Box.Body>
+                </Box>
+            </div>
         )
     }
 }
